325e01f7cba9dc45464e9e57f51711a7951d27d2
browissimo <browissimo@gmail.com>
2022-09-28 23:06:01 +0300

    index page added

---
+++ WebApplication1/wwwroot/js/index.js
@@ -0,0 +1,17 @@
+﻿window.addEventListener('DOMContentLoaded', () => {
+    const menu = document.querySelector('.main_menu'),
+        menuItem = document.querySelectorAll('.main_menu_item'),
+        hamburger = document.querySelector('.hamburger');
+
+    hamburger.addEventListener('click', () => {
+        hamburger.classList.toggle('hamburger_active');
+        menu.classList.toggle('main_menu_active');
+    });
+
+    menuItem.forEach(item => {
+        item.addEventListener('click', () => {
+            hamburger.classList.toggle('hamburger_active');
+            menu.classList.toggle('main_menu_active');
+        })
+    })
+})
