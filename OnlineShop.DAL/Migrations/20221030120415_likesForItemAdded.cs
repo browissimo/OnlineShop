78c3b3668cdaa7f496702310005a7ba25586ad5a
browissimo <browissimo@gmail.com>
2022-10-30 15:55:36 +0300

    just save

---
+++ OnlineShop.DAL/Migrations/20221030120415_likesForItemAdded.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace OnlineShop.DAL.Migrations
+{
+    public partial class likesForItemAdded : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "likesCount",
+                table: "Items",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 1L,
+                column: "Email",
+                value: "Browissimo@mail.ru");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "likesCount",
+                table: "Items");
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 1L,
+                column: "Email",
+                value: null);
+        }
+    }
+}
